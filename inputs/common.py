41bfb8c25890bd32aaf94d9ff771506fb1a712a0
Ryan Stocks <ryan.stocks00@gmail.com>
2025-09-30 15:02:46 +1000

    Autodecompress

--- inputs/common.py
+++ inputs/common.py
@@ -29,6 +29,24 @@ def get_all_graphene_isomers(
     folder: Path = Path(__file__).parent / "compas3x-xyzs",
 ) -> list[GrapheneIsomer]:
     """Get all GrapheneIsomer objects from .xyz files in a folder."""
+    if not folder.exists():
+        # try unzipping compas-3x.tar.gz from the parent directory
+        import tarfile
+        import shutil
+        import sys
+        parent_folder = folder.parent
+        tar_path = parent_folder / "compas-3x.tar.gz"
+        if not tar_path.exists():
+            print(f"Error: {tar_path} does not exist.", file=sys.stderr)
+            sys.exit(1)
+        with tarfile.open(tar_path, "r:gz") as tar:
+            tar.extractall(path=parent_folder)
+        if not folder.exists():
+            print(f"Error: {folder} does not exist after extracting {tar_path}.", file=sys.stderr)
+            sys.exit(1)
+
+
+
     xyz_files = get_all_xyzs(folder)
     return [GrapheneIsomer(xyz) for xyz in xyz_files]
 
